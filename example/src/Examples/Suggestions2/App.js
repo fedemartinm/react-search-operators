0b545f63b11ec71b5c88502708ffb3f14a4ca779
fmartin <fedemartin94@gmail.com>
2020-04-27 23:00:51 -0300

    Added suggestions example

---
+++ example/src/Examples/Suggestions2/App.js
@@ -0,0 +1,48 @@
+import 'react-search-operators/dist/index.css'
+import './app.css'
+
+import React, { useState } from 'react'
+
+import Search from 'react-search-operators'
+
+const App = () => {
+  const [text, setText] = useState('')
+  // Hide if match operator present
+  const suggestFilter = (_, searchState) =>
+    !searchState.parsed.filters.some((filter) => filter.type === 'match')
+  //Suggest user to filter origin of search
+  const suggestions = [
+    {
+      show: suggestFilter,
+      text: (_, { text }) => `${text} - in all github`,
+      repo: 'all',
+    },
+    {
+      show: suggestFilter,
+      text: (_, { text }) => `${text} - in this repo`,
+      repo: 'this',
+    },
+  ]
+  //On select sugget, change search applying operation
+  const select = (e) => {
+    console.log('Entre', e)
+    setText(`in:${e.repo} ${text}`)
+  }
+  //Tell parser that in is a valid match operand
+  const options = {
+    keys: ['in'],
+  }
+  return (
+    <div className='app'>
+      <Search
+        text={text}
+        onTextChange={setText}
+        suggestions={suggestions}
+        parserOptions={options}
+        onSelect={select}
+      />
+    </div>
+  )
+}
+
+export default App
---
+++ example/src/Examples/Suggestions2/index.js
@@ -0,0 +1,16 @@
+import App from './App'
+
+const Suggestions2 = App
+Suggestions2.title = 'Controlled, advanced suggestions'
+Suggestions2.description =
+  'In this example we use a dynamic condition; suggestions are displayed \
+   according to the applied operators. If user select a suggestion, we \
+   apply the related operator. To achieve this, we use the component \
+  in a controlled way.'
+
+Suggestions2.source =
+  'https://github.com/fedemartinm/react-search-operators/blob/master/example/src/Examples/Suggestions2/App.js'
+Suggestions2.raw =
+  'https://raw.githubusercontent.com/fedemartinm/react-search-operators/master/example/src/Examples/Suggestions2/App.js'
+
+export default Suggestions2
