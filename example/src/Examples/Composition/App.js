befd2ef9400eb36c1047865b47e9f79e97ac1f0b
fmartin <fedemartin94@gmail.com>
2020-04-28 01:28:34 -0300

    added composition example

---
+++ example/src/Examples/Composition/App.js
@@ -0,0 +1,26 @@
+import './app.css'
+
+import React from 'react'
+import Search from 'react-search-operators'
+import Suggestion from './Suggestion'
+import styles from './theme.module.scss'
+
+const App = () => {
+  const suggestions = [
+    {
+      show: true,
+      text: 'Open in new window',
+    },
+  ]
+  return (
+    <div className='app'>
+      <Search
+        theme={styles}
+        suggestions={suggestions}
+        suggestionAs={Suggestion}
+      />
+    </div>
+  )
+}
+
+export default App
---
+++ example/src/Examples/Composition/index.js
@@ -0,0 +1,14 @@
+import App from './App'
+
+const Composition = App
+Composition.title = 'Composition & Style'
+Composition.description =
+  "It' s possible to override default styles (theme prop) \
+  and default components (as and suggestionAs). In this example we overwrite \
+  the theme by adding light and dark modes."
+Composition.source =
+  'https://github.com/fedemartinm/react-search-operators/blob/master/example/src/Examples/Composition/App.js'
+Composition.raw =
+  'https://raw.githubusercontent.com/fedemartinm/react-search-operators/master/example/src/Examples/Composition/App.js'
+
+export default Composition
