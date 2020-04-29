521eaf7728f44a0521b6e03cbc2da8b66dc0c6f3
fmartin <fedemartin94@gmail.com>
2020-04-28 22:12:19 -0300

    Updates

---
+++ precache-manifest.e539aca7d24acc33f90c239814ea1fe5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4553ea72459135f1c1dd58e460733ee1",
+    "url": "/react-search-operators/index.html"
+  },
+  {
+    "revision": "464d6c6d4384ce0adf9d",
+    "url": "/react-search-operators/static/css/main.a7516575.chunk.css"
+  },
+  {
+    "revision": "d77034bed4a423b267a3",
+    "url": "/react-search-operators/static/js/2.4a711d88.chunk.js"
+  },
+  {
+    "revision": "c55db850dbdf6c8134c3f97f199421a1",
+    "url": "/react-search-operators/static/js/2.4a711d88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "464d6c6d4384ce0adf9d",
+    "url": "/react-search-operators/static/js/main.1764aafa.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "/react-search-operators/static/js/main.1764aafa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "433298516b369cba4616",
+    "url": "/react-search-operators/static/js/runtime-main.dc8b6b93.js"
+  }
+]);
